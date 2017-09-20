82e060d4f6728aa2e371361b3c6aa62b69570310
Nikhiljoshi28 <nikhiljoshi1nikhil@gmail.com>
2017-09-20 23:16:05 +0530

    error fix query_processing

--- rank-retrieval-model/query_processing.py
+++ rank-retrieval-model/query_processing.py
@@ -3,17 +3,18 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 class query_reduction:
-    def reducedQuery(self):
-        query = "and and and rank of retrieval and model in in in information retrieval for searching of of "
+    def reducedQuery_stopwords(self):
+        query = "rank of the retrieval this is and model in information retrieval for searching of and"
         words = word_tokenize(query)
         stop_words = set(stopwords.words("english"))
         filtered_query = []
         for w in range(len(words)):
             if words[w] not in stop_words:
                 filtered_query.append(words[w])
-            elif w==len(words)-1 and words[w-1]==words[w]:
+            elif w>0 and w<len(words)-1 and words[w] in stop_words and (words[w-1] in stop_words or words[w+1] in stop_words):
                 filtered_query.append(words[w])
-            elif words[w] in stop_words and (words[w+1] in stop_words or words[w-1] in stop_words) :
+            elif w==0 and words[w] in stop_words and words[w+1] in stop_words:
                 filtered_query.append(words[w])
-
+            elif w==len(words)-1 and words[w] in stop_words and words[w-1] in stop_words:
+                filtered_query.append(words[w])
         return filtered_query
