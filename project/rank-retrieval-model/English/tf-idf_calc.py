0fc9252baa3f23b1663d1b6785b20dd06b3f3ccf
eagle-12 <nikhil29091997@gmail.com>
2017-10-02 16:05:48 +0530

    tf-idf-eng

--- project/rank-retrieval-model/English/tf-idf_calc.py
+++ project/rank-retrieval-model/English/tf-idf_calc.py
@@ -1,7 +1,7 @@
 import json
 import math
 
-class Tf-Idf:
+class Tf_Idf:
 	def __init__(self,addr_tf,addr_idf):
 		with open(addr_tf) as json_data:
 			self.tf = json.load(json_data)
@@ -15,16 +15,17 @@ class Tf-Idf:
 		for term in self.tf:
 			self.tf[term]=math.log(1+self.tf[term],10)*(self.idf[term])
 
-	def tf_idf_file(self,addr_tf):
-		with open(addr_tf,'w') as score:
+	def tf_idf_file(self,new_tf):
+		with open(new_tf,'w') as score:
 			json.dump(self.tf,score,ensure_ascii=False)
 
 
 if __name__=='main':
 
 	addr = '/home/nikhil/wikipediasearch/termfrequencydocs'
+	addr_idf = '/home/nikhil/wikipediasearch/idf'
 	files = os.listdir('/home/nikhil/wikipediasearch/termfrequencydocs')
 	for filename in files:
 		score = Tf-Idf(addr+filename,idf_addr)
 		score.tf_idf_calc()
-		score.tf_idf_file(addr+filename)
+		score.tf_idf_file(addr_idf+filename)
