0ef47ea97e564b1e170bfd99cd117affacae9ed4
eagle-12 <nikhil29091997@gmail.com>
2017-10-02 23:04:04 +0530

    changes in queryprocessing

--- project/rank-retrieval-model/English/tf-idf_calc.py
+++ project/rank-retrieval-model/English/tf-idf_calc.py
@@ -1,31 +1,44 @@
 import json
 import math
+import os
+import glob
 
 class Tf_Idf:
+
 	def __init__(self,addr_tf,addr_idf):
+		"""load the file containing dictionary of term freq and file containing Inverted doc freq"""
 		with open(addr_tf) as json_data:
 			self.tf = json.load(json_data)
 			json_data.close()
 
-		with open(addr_tf) as json_data:
+		with open(addr_idf) as json_data:
 			self.idf = json.load(json_data)
 			json_data.close()
 
 	def tf_idf_calc(self):
+		"""for each term present in the doc multiply each of the term's freq with inverted doc freq"""
 		for term in self.tf:
-			self.tf[term]=math.log(1+self.tf[term],10)*(self.idf[term])
+			try:
+				self.tf[term]=math.log(1+self.tf[term],10)*(self.idf[term])
+			except Exception as e:
+				print(term,e)
+				
+	def tfidffile(self,new_tf):
+		"""store the dictionary of tf-idf values in the form of a json object"""
+		try:
+			with open(new_tf,'w') as score:
+				json.dump(self.tf,score,ensure_ascii=False)
+		except Exception as e:
+			print (e)
 
-	def tf_idf_file(self,new_tf):
-		with open(new_tf,'w') as score:
-			json.dump(self.tf,score,ensure_ascii=False)
 
-
-if __name__=='main':
-
-	addr = '/home/nikhil/wikipediasearch/termfrequencydocs'
-	addr_idf = '/home/nikhil/wikipediasearch/idf'
-	files = os.listdir('/home/nikhil/wikipediasearch/termfrequencydocs')
+if __name__=='__main__':
+	addr = '/home/tex/Documents/IR/Final_Output1000/'
+	addr_idf = '/home/tex/Documents/IR/Inverted_Index/idf.txt'
+	new_file = '/home/tex/Documents/IR/tf_idf/'
+	files = os.listdir('/home/tex/Documents/IR/Final_Output1000/')
 	for filename in files:
-		score = Tf-Idf(addr+filename,idf_addr)
+		print(filename)
+		score = Tf_Idf(addr+filename,addr_idf)
 		score.tf_idf_calc()
-		score.tf_idf_file(addr_idf+filename)
+		score.tfidffile(new_file+filename)
