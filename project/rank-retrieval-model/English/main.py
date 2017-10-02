9804f32ccb0fc080c6b688d009a5f5c274afe0a1
eagle-12 <nikhil29091997@gmail.com>
2017-10-02 14:33:16 +0530

    query processing and inverted_idx and idf

--- project/rank-retrieval-model/English/main.py
+++ project/rank-retrieval-model/English/main.py
@@ -2,6 +2,8 @@ from query_processing import query_reduction as qr
 from document_processing import document_reduction as dr
 from indexing import indexing as indx
 
+import json
+
 
 runQuery = qr()
 filter_query = runQuery.reducedQuery_stopwords()
@@ -19,3 +21,38 @@ runDoc.decReduction(path)
 runIndexing = indx()
 runIndexing.file_indexing()
 """
+
+if __name__=='__main__':
+	input_query = raw_input()
+	query = input_query.split()
+	with open('inverted_index.txt') as json_data:
+		inverted_idx = json.load(json_data)
+		json_data.close()
+
+	docs = []
+	siz = len(query)
+
+	idx = 0
+
+	for i in range(1,siz):
+		if len(inverted_index[query[i]])<len(inverted_index[query[idx]]):
+			idx = i
+
+	for i in range(0,len(inverted_index[query[idx]])):
+		docs.append(inverted_index[query[idx]][i])
+
+	# returns the list of docs corresponding to give query assuming there is no need for any spell correction	
+	# docs is the required list of documents
+	for i in range(0,siz):
+		if i!=idx:
+			ans = []
+			for j in range(0,len(docs)):
+				for k in range(0,len(inverted_index[query[i]])):
+					if docs[j]==inverted_index[query[i]][k]:
+						ans.append(docs[j])
+
+			docs[:] = []
+					
+			for j in range(0,len(ans)):
+				docs.append(ans[j])
+
